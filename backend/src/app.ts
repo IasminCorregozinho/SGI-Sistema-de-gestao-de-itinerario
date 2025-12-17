a22bec21dd28045e1800201b406ed1b07899aaf4
FabianaBios <44126832+BIOSAleandro@users.noreply.github.com>
2025-12-17 15:16:10 -0300

    fix-estrutura-integracao

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 app.use('/ativos', ativoRoutes);
 app.use('/users', userRoutes);
 
--- frontend/script.js
+++ frontend/script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         try {
-            const response = await fetch('http://localhost:3000/users/login', {
+            const response = await fetch('/users/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
