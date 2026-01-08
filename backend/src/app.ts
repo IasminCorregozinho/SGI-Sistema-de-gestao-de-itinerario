08c9357107fec178f36fe46904ead1aa28e41cb0
Fabiana Chaves <fabianasanves@gmail.com>
2026-01-08 19:48:12 -0300

    feat/edicao-perfis

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,6 @@
+// App Configuration
+// * Configura a aplicação Express, middlewares e rotas principais.
+
 import express from 'express';
 import ativoRoutes from './routes/ativoRoutes';
 import path from 'path';
@@ -7,9 +10,13 @@ import cors from 'cors';
 
 const app = express();
 
+// Configuração de Middlewares
+// CORS, JSON Parser e Arquivos Estáticos
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../../frontend')));
+
+// Definição de Rotas
 app.use('/ativos', ativoRoutes);
 app.use('/users', userRoutes);
 
